ee4bba07c6fc48ac9070f1db2ec16eda4cf2dc37
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-18 22:20:39 -0300

    HashSet

--- projetos/src/Collections/SistemaFinanceiro.java
+++ projetos/src/Collections/SistemaFinanceiro.java
@@ -1,45 +1,51 @@
 package Collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class SistemaFinanceiro {
 
     public static void main(String[] args) {
-//        Cliente destinatario = new Cliente();
-//        destinatario.setNome("Beltrano");
-//        destinatario.setSobrenome("De tal");
-//        Conta contaDestino = new ContaCorrente(destinatario);
-//
-//        CaixaEletronico caixaEletronico = new CaixaEletronico();
-//        caixaEletronico.autenticar("2");
-//        caixaEletronico.exibirSaldo();
-//        caixaEletronico.efetuarDeposito(1000);
 //        caixaEletronico.efetuarSaque(20);
 //        caixaEletronico.efetuarTransferencia(contaDestino, 40);
 //        caixaEletronico.encerrarUtilizacao();
-        Cliente titular = new Cliente("Fabio", "Silva");
-        Cliente outroTitular = new Cliente("Fulano", "De tal");
-        Cliente maisUmTitular = new Cliente("Beltrano", "De tal");
+//        Cliente titular = new Cliente("Fabio", "Silva");
+//        Cliente outroTitular = new Cliente("Fulano", "De tal");
+//        Cliente maisUmTitular = new Cliente("Beltrano", "De tal");
+//
+//        ContaConjunta contaConjunta = new ContaConjunta(titular);
+//
+//        // Da para usar herança para adicionar elementos que não sejam do tipo Cliente diretamente
+//        List<Cliente> outrosTitulares = new Vector<Cliente>();
+//        outrosTitulares.add(outroTitular);
+//        outrosTitulares.add(outroTitular);
+//        outrosTitulares.add(maisUmTitular);
+//        contaConjunta.setOutrosTitulares(outrosTitulares);
+//
+//        for (Cliente titulares : contaConjunta.getOutrosTitulares()) {
+//            System.out.println("Nome: " + titulares.getNome() + ", sobrenome: " + titulares.getSobrenome());
+//        }
+//
+//        for (int i = 0; i < outrosTitulares.size(); i++) {
+//            Cliente titulares = outrosTitulares.get(i);
+//            System.out.println("Nome: " + titulares.getNome() + ", sobrenome: " + titulares.getSobrenome());
+//        }
 
-        ContaConjunta contaConjunta = new ContaConjunta(titular);
+        // Classes wrappers encapsulam os comportamentos dos tipos primitivos
+        // Ordem não importa, não pode haver repetições e todos valores são do mesmo tipo
+        Set<Integer> numeros = new HashSet<Integer>();
 
-        // Da para usar herança para adicionar elementos que não sejam do tipo Cliente diretamente
-        List<Cliente> outrosTitulares = new Vector<Cliente>();
-        outrosTitulares.add(outroTitular);
-        outrosTitulares.add(outroTitular);
-        outrosTitulares.add(maisUmTitular);
-        contaConjunta.setOutrosTitulares(outrosTitulares);
+        numeros.add(2);
+        numeros.add(10);
+        numeros.add(1);
+        numeros.add(25);
+        numeros.add(2);
+        numeros.add(12);
+        numeros.add(3);
 
-        for (Cliente titulares : contaConjunta.getOutrosTitulares()) {
-            System.out.println("Nome: " + titulares.getNome() + ", sobrenome: " + titulares.getSobrenome());
+        for (Integer numero: numeros) {
+            System.out.println(numero);
         }
 
-        for (int i = 0; i < outrosTitulares.size(); i++) {
-            Cliente titulares = outrosTitulares.get(i);
-            System.out.println("Nome: " + titulares.getNome() + ", sobrenome: " + titulares.getSobrenome());
-        }
+
     }
 }
