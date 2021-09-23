6e84146bd892c2bf0520001933e329d3c039c9d3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-22 21:23:42 -0300

    Cofigurações iniciais do projeto Save The Roupa

---
+++ projetos/src/SaveTheRoupa/GuardaVolumes.java
@@ -0,0 +1,13 @@
+package SaveTheRoupa;
+
+import java.util.*;
+
+public class GuardaVolumes {
+
+    private Map<Integer, List<Peca>> secoes = new HashMap<>();
+    private Integer identificador = 1;
+
+    public Integer guardarPecas(List<Peca> listaDePecas) {
+        return 0;
+    }
+}
---
+++ projetos/src/SaveTheRoupa/Peca.java
@@ -0,0 +1,22 @@
+package SaveTheRoupa;
+
+public class Peca {
+    private String marca;
+    private String modelo;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
